fac04e506a8df3c7d9b72b7b3b7f40266bc54164
Nikolai <109143314+4eever@users.noreply.github.com>
2024-06-21 02:13:33 +0300

    Создание схемы бд(code first) + миграция (DAL+бд) (#1)
    * Создание схемы бд(code first) + миграция (DAL+бд)
    
    * Database v2

---
+++ DataAccessLayer/Migrations/20240620225825_Initial.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserPassword",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductrId",
+                table: "Products",
+                newName: "ProductId");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "UserHashedPassword",
+                table: "Users",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserHashedPassword",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductId",
+                table: "Products",
+                newName: "ProductrId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserPassword",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
