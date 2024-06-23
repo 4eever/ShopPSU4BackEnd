a232621561e61ae1e27831236ca1a0db361ff636
hxkeroo <shulga.anton2003@gmail.com>
2024-06-23 20:41:39 +0300

    Add validotors

---
+++ BusinessAccessLayer/Validators/ValidatorConst.cs
@@ -0,0 +1,7 @@
+﻿namespace BusinessAccessLayer.Validators
+{
+    internal class ValidatorConst
+    {
+        public const string emptyErrorMessage = "The '{PropertyName}' field cannot be empty";
+    }
+}
